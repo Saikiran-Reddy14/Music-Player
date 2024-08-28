1f9c9b8bf86d3c471c46fc4b597e5aba36b79214
Saikiran-Reddy14 <saikian.kadari14@gmail.com>
2024-08-28 18:04:24 +0530

    added songs list component

--- src/SongsList.js
+++ src/SongsList.js
@@ -1,7 +1,126 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SongsList = () => {
-  return <div>SongsList</div>;
+const SongsList = ({
+  setCurrentTopSong,
+  setPlayingTopTrack,
+  search,
+  setSearch,
+  songs,
+  topTracks,
+  setIndex,
+  setCurrentSong,
+  setPlaying,
+  index,
+}) => {
+  const [activeTab, setActiveTab] = useState("default");
+
+  const filteredSongs = songs.filter(
+    (song) =>
+      song.name.toLowerCase().includes(search.toLowerCase()) ||
+      song.artist.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const filteredTopTracks = topTracks.filter(
+    (song) =>
+      song.name.toLowerCase().includes(search.toLowerCase()) ||
+      song.artist.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="songs-list">
+        <nav>
+          <span
+            onClick={() => {
+              setActiveTab("default");
+              setIndex(null);
+            }}
+            style={{ opacity: activeTab !== "default" ? 0.6 : 1 }}
+          >
+            For You
+          </span>
+          <span
+            onClick={() => {
+              setActiveTab("top");
+              setIndex(null);
+            }}
+            style={{ opacity: activeTab !== "top" ? 0.6 : 1 }}
+          >
+            Top Tracks
+          </span>
+        </nav>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search Song, Artist"
+        />
+        <ul className="songs">
+          {activeTab === "default" && filteredSongs.length > 0 ? (
+            filteredSongs.map((song, ind) => (
+              <li
+                key={song.id}
+                className="song-card"
+                onClick={() => {
+                  setIndex(ind);
+                  setCurrentSong(song);
+                  setPlaying(true);
+                }}
+                style={{
+                  border: index === ind ? "1px solid black" : "",
+                  backgroundColor: index === ind ? "#aaa" : "",
+                  color: index === ind ? "black" : "",
+                }}
+              >
+                <img
+                  src={`https://cms.samespace.com/assets/${song.cover}`}
+                  alt={song.name}
+                  width={"50px"}
+                  height={"50px"}
+                />
+                <div className="about-song">
+                  <p className="name">{song.name}</p>
+                  <p className="artist">{song.artist}</p>
+                </div>
+              </li>
+            ))
+          ) : activeTab === "top" && filteredTopTracks.length > 0 ? (
+            filteredTopTracks.map((song, ind) => (
+              <li
+                key={song.id}
+                className="song-card"
+                onClick={() => {
+                  setIndex(ind);
+                  setCurrentTopSong(song);
+                  setPlayingTopTrack(true);
+                }}
+                style={{
+                  border: index === ind ? "1px solid black" : "",
+                  backgroundColor: index === ind ? "#aaa" : "",
+                  color: index === ind ? "black" : "",
+                }}
+              >
+                <img
+                  src={`https://cms.samespace.com/assets/${song.cover}`}
+                  alt={song.name}
+                  width={"50px"}
+                  height={"50px"}
+                />
+                <div className="about-song">
+                  <p className="name">{song.name}</p>
+                  <p className="artist">{song.artist}</p>
+                </div>
+              </li>
+            ))
+          ) : (
+            <p>No songs found...</p>
+          )}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default SongsList;
